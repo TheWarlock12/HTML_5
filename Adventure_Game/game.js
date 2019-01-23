a31e6f6fa1edf7e5668ff41a5cf3a4a9b9149dd1
Unknown <micah.eastwood@gmail.com>
2019-01-23 10:27:49 -0700

    Added index and game.js files

---
+++ Adventure_Game/game.js
@@ -0,0 +1,19 @@
+//single line comment 
+/* 
+
+multiline commment
+
+*/
+
+    // document.write("<h1>I can't wait to get to some <em>real</em> coding.</h1>");
+
+    // alert ("Danger, Will Robinson.");
+
+    // confirm("Do you like pokemon?");
+
+    // prompt ("What type of pokemon do you like?");
+var pc = prompt("What is your name?");
+
+confirm("So your name is "+pc);
+
+
